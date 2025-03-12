30f77cd20b7b296641767afcc0261de349b014e7
melikeaygul <melikeaygul0810@gmail.com>
2025-03-12 14:24:53 +0100

    Aufgabe 2 +3

--- funktionen.dart
+++ funktionen.dart
@@ -1,9 +1,31 @@
 void main() {
+  //Aufgabe1
   printNumber(9);
   printNumber(7);
   printNumber(3);
+
+  //Aufgabe2
+  triplePrint("Lala");
+
+  //Aufgabe3
+  String firstName = "JOHNNY";
+  String lastName = "DEPP";
+  reverseInitials(firstName, lastName);
 }
 
+//Aufgabe 1
 void printNumber(int x) {
   print(x);
+}
+
+//Aufgabe 2
+void triplePrint(String name) {
+  print((name + " ") * 3);
+}
+
+//Aufgabe 3
+void reverseInitials(String first, String last) {
+  print(first);
+  print(last);
+  print("${first[first.length - 1]}. ${last[last.length - 1]}. ");
 }
